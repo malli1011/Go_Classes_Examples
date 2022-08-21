cb1782f6567cd34de8bfff437e663f3360a2ebd1
Sakthi Subramaniam <v-saksubramania@hotels.com>
2022-08-21 11:55:11 +0100

    tests

--- structs.go
+++ structs.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"encoding/json"
 	"fmt"
 	"time"
 )
@@ -8,7 +9,7 @@ import (
 type Person struct {
 	Name      string    `json:"name"`
 	DoB       time.Time `json:"dob"`
-	PhNumbers []string  `json:"phone omitempty"`
+	PhNumbers []string  `json:"phone,omitempty"`
 }
 
 type tag1 struct {
@@ -38,6 +39,29 @@ func structsDemo() {
 		Name string `json:bar`
 	}{"Arjun"}
 
+	s2 := time.Date(2000, 2, 1, 12, 30, 0, 0, time.UTC)
 	fmt.Println(m1, m2)
+	p := Person{
+		"Malli",
+		s2,
+		[]string{"9876543210"},
+	}
+
+	fmt.Println(p)
+
+	b2, _ := json.Marshal(p)
+
+	fmt.Println(string(b2))
+
+	p2 := Person{
+		"Malli",
+		s2,
+		[]string{},
+	}
+
+	fmt.Println(p2)
+
+	b3, _ := json.Marshal(p2)
+	fmt.Println(string(b3))
 
 }
